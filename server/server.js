cad85e89c459028a5bbc8fcf26f2bc24273b9180
qqqy <frogasaurus@hotmail.com>
2019-06-07 16:44:37 -0600

    forgot to serve static

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(session({
   resave: false ,
   saveUninitialized: false
 }))
-// app.use( express.static( `${__dirname}/../build` ) );
+app.use( express.static( `${__dirname}/../build` ) );
 
 // TEST ENDPOINTS //
 
